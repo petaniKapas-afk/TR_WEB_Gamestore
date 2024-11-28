e4a4e24a18689c3ebfa3ed1e644667861bc18aea
petaniKapas-afk <petanikapas421@GMAIL.COM>
2024-11-28 16:49:34 +0700

    sepatu jaran

--- verify_payments.php
+++ verify_payments.php
@@ -24,7 +24,7 @@ if (!$result) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Verify Payments</title>
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
     <h1>Verify Payments</h1>
